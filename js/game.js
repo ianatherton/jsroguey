aa0f8ea4ae6784700f089f8850fd78aba7c91f7b
Ian Atherton <atherton.i@gmail.com>
2023-11-09 13:01:50 -0800

    render single character as player

--- js/game.js
+++ js/game.js
@@ -1,5 +1,51 @@
-// Get reference to game area div
-let gameArea = document.getElementById("game-area");
+const gameArea = document.getElementById('game-area')
+// Player class
+class Player {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.character = '&';
+  }
 
-// Draw message in game area 
-gameArea.innerHTML = "Hello Roguelike!";
+  draw() {
+    let gameScreen = '';
+    
+    // Add newlines for blank rows
+    for (let i = 0; i < 20; i++) {
+      gameScreen += '\n';
+    }
+
+    // Add player character
+    gameScreen += this.character;
+
+    // Draw game screen
+    gameArea.innerHTML = gameScreen;
+
+  }
+
+  move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+    this.draw(); 
+  }
+
+}
+
+// Create player
+const player = new Player();
+
+// Draw initial screen
+player.draw();
+
+// Movement
+document.addEventListener('keydown', e => {
+  if (e.key === 'ArrowUp') {
+    player.move(0, -1);
+  } else if (e.key === 'ArrowDown') {
+    player.move(0, 1);
+  } else if (e.key === 'ArrowLeft') {
+    player.move(-1, 0);
+  } else if (e.key === 'ArrowRight') {
+    player.move(1, 0);
+  }
+});
